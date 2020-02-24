b7b81b0751c50c034e576bc577c593b6cbf5b596
Victor M. Macias Ariza <blanvam@gmail.com>
2020-02-24 21:06:30 +0100

    added GoogleService plist for ios and running

--- ios/dogphin/AppDelegate.m
+++ ios/dogphin/AppDelegate.m
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#import <Firebase.h>
+
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -15,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"dogphin"
