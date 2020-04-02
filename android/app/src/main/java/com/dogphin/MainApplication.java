1820eb07bd428cd1fb971eb4577d967d1906c3e1
Victor M. Macias Ariza <blanvam@gmail.com>
2020-04-02 22:59:34 +0200

    Added @react-native-community/geolocation and updated react-native

--- android/app/src/main/java/com/dogphin/MainApplication.java
+++ android/app/src/main/java/com/dogphin/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
 // import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
 // import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
