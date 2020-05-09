a6b0fe9e6726650883ec06f5d8a3abe04258df88
Victor M. Macias Ariza <blanvam@gmail.com>
2020-05-09 12:03:33 +0200

    Optionals permissions for unavailable

--- src/home/permission/permissionsChecker.js
+++ src/home/permission/permissionsChecker.js
@@ -1,13 +1,19 @@
 import { checkMultiple, request, RESULTS } from 'react-native-permissions'
 
 export function permissionsChecker(permissions, handleGranted) {
-  checkMultiple(permissions).then(response => {
+  let requestPermissions = permissions["request"]
+  let optionalPermissions = permissions["optional"]
+  checkMultiple(requestPermissions).then(response => {
     let granted = true
-    for (const perm of permissions) {
+    for (const perm of requestPermissions) {
       switch (response[perm]) {
         case RESULTS.UNAVAILABLE:
           console.log(`The permission ${perm} is not available (on this device / in this context)`)
-          granted = false
+          if (optionalPermissions.includes(perm)) {
+            granted = granted && true
+          } else {
+            granted = false
+          }
           break;
         case RESULTS.DENIED:
           console.log(`The permission ${perm} has not been requested / is denied but requestable`)
