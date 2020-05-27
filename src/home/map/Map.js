1bee7b7c9b6635baabc1674d1268a3d259126e6b
Victor M. Macias Ariza <blanvam@gmail.com>
2020-05-28 00:45:09 +0200

    Refactor map and added watch to move position to markers or notifications

--- src/home/map/Map.js
+++ src/home/map/Map.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { StyleSheet, Dimensions } from 'react-native'
 import { View } from 'native-base'
@@ -18,100 +18,94 @@ const styles = StyleSheet.create({
 const {height, width} = Dimensions.get('window');
 
 
-class Map extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      region: {
-        latitude: props.location.latitude,
-        longitude: props.location.longitude,
-        latitudeDelta: 0.5,
-        longitudeDelta: 0.5,
-      },
-      timestamp: null,
-      watchID: null
+const Map = props => {
+
+  const [region, setRegion] = useState({
+    latitude: props.location.latitude,
+    longitude: props.location.longitude,
+    latitudeDelta: 0.5,
+    longitudeDelta: 0.5,
+  })
+  const [watchID, setWatchID] = useState(null)
+  const [mapRef, setMapRef] = useState(null)
+ 
+  useEffect(() => { 
+    console.log(`${props.permissionsGranted} && ${watchID} && ${!watchID} && ${watchID == null}`)
+    if (props.permissionsGranted && !watchID) {
+      set_geolocation()
+    } else if (watchID) {
+      move(props.location.latitude, props.location.longitude)
     }
-  }
+    return (() => {
+      watchID && Geolocation.clearWatch(watchID)
+    })
+  }, [props.permissionsGranted, props.location])
 
-  componentDidMount() {
-    if (this.props.permissionsGranted) {
-      this._set_geolocation()
-    }
-  }
-
-  componentDidUpdate(prevProps){
-    if (this.props.permissionsGranted && this.props.permissionsGranted !== prevProps.permissionsGranted) {
-        this._set_geolocation()
-      }
-  }
-
-  componentWillUnmount() {
-    this.state.watchID != null && Geolocation.clearWatch(this.state.watchID)
-  }
-
-  _set_geolocation() {
+  set_geolocation = () => {
+    console.log('setting location')
     Geolocation.setRNConfiguration({"authorizationLevel": "always"})
     Geolocation.getCurrentPosition(
-      position => {this._move(position.coords.latitude, position.coords.longitude)},
+      position => { move(position.coords.latitude, position.coords.longitude)},
       error => console.log('Error getCurrentPosition', JSON.stringify(error)),
       {enableHighAccuracy: true, timeout: 10000, maximumAge: 10000},
     )
     let watchID = Geolocation.watchPosition(
-      position => { this._move(position.coords.latitude, position.coords.longitude)},
+      position => { move(position.coords.latitude, position.coords.longitude)},
       error => console.log('Error watchPosition', JSON.stringify(error)),
       {enableHighAccuracy: true, timeout: 10000, maximumAge: 10000, distanceFilter: 200},
     )
-    this.setState({ watchID })
+    setWatchID(watchID)
   }
 
-  _move(latitude, longitude) {
+  move = (latitude, longitude) => {
+    console.log(`moving to lat ${latitude} - lon ${longitude}`)
     let newRegion = {
       latitude: latitude,
       longitude: longitude,
-      latitudeDelta: this.state.region.latitudeDelta,
-      longitudeDelta: this.state.region.longitudeDelta
+      latitudeDelta: region.latitudeDelta,
+      longitudeDelta: region.longitudeDelta
     }
-    this.mapRef.animateToRegion(newRegion, 5000)
+    mapRef.animateToRegion(newRegion, 5000)
   }
 
-  _set_region(region) {
-    this.setState({ region })
+  set_region = (region) => {
+    setRegion(region)
     let actualLocation = {latitude: region.latitude, longitude: region.longitude}
     // TODO send to firestore actual location (new firestore.GeoPoint(53.483959, -2.244644))
     let location = new firestore.GeoPoint(region.latitude, region.longitude)
     userServices.update(
-      this.props.user.email,
+      props.user.email,
       {currentLocation: location},
-      () => { this.props.updateLocation(actualLocation) }
+      () => { props.updateLocation(actualLocation) }
     )
   }
 
-  render() {
-    return (
-      <View style={{height:height, width: width}}>
-        <MapView
-          ref={(ref) => this.mapRef = ref}
-          style={[styles.map]}
-          mapType="hybrid"
-          //customMapStyle={mapStyle}
-          region={this.state.region}
-          onRegionChangeComplete={(v) => this._set_region(v)}
-          showsUserLocation={true}
-          followsUserLocation={true}
-          loadingEnabled={true}
-        >
-          {this.props.markers.map(marker => (
-            <Marker
-              key={marker.id}
-              coordinate={marker.latlng}
-              title={marker.title}
-              description={marker.description}
-            />
-          ))}
-        </MapView>
-      </View>
-    );
-  }
+  
+  return (
+    <View style={{height:height, width: width}}>
+      <MapView
+        ref={(ref) => setMapRef(ref)}
+        style={[styles.map]}
+        mapType="hybrid"
+        //customMapStyle={mapStyle}
+        region={region}
+        onRegionChangeComplete={(v) => set_region(v)}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        loadingEnabled={true}
+      >
+        {props.markers.map(marker => (
+          <Marker
+            key={marker.id}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
+    </View>
+  )
+
 }
 
 const mapStateToProps = state => {
