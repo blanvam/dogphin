3f25a7c380a3f9b8f91f2648d6e39d685761d8d1
Victor M. Macias Ariza <blanvam@gmail.com>
2020-07-20 23:18:11 +0200

    Show hide secureTextEntry form item

--- src/components/FormItem.js
+++ src/components/FormItem.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Item, Label, Text, Row, Input, Button, Icon } from 'native-base'
 
-const LabelError = (props) => {
+const LabelError = props => {
   if (props.obligatory) {
     return (<Label>{props.label} <Text style={{color: '#d9534f'}}>*</Text></Label>)
   } else {
@@ -9,54 +9,57 @@ const LabelError = (props) => {
   }
 }
 
-export default class FormItem extends Component {
-  render() {
-    if (this.props.error) {
+export default FormItem = props => {
+
+  const [hideValue, setHideValue] = useState(true)
+
+  useEffect(() => { }, [hideValue])
+
+  getButton = () => {
+    if (props.disabled) {
       return (
-        <Item stackedLabel error>
-          <LabelError label={this.props.label} obligatory={this.props.obligatory} />
-          <Row>
-            <Input 
-              value={this.props.value}
-              secureTextEntry={this.props.secureTextEntry}
-              onChangeText={(v) => this.props.onChangeText(v)}
-              keyboardType={this.props.keyboardType || 'default'}
-            />
-            <Button danger transparent>
-              <Icon type="MaterialCommunityIcons" name="close-circle" />
-            </Button>
-          </Row>
-        </Item> )
-    } 
-    if (this.props.disabled) {
-      return (
-      <Item stackedLabel>
-        <LabelError label={this.props.label} obligatory={this.props.obligatory} />
-        <Row>
-            <Input 
-              disabled
-              placeholder={this.props.placeholder} 
-              value={this.props.value} 
-              secureTextEntry={this.props.secureTextEntry} 
-            />
-            <Button dark transparent>
-              <Icon type="MaterialIcons" name="block" />
-            </Button>
-        </Row>
-      </Item>
+        <Button dark transparent>
+          <Icon type="MaterialIcons" name="block" />
+        </Button>
       )
     }
-    return (
-      <Item stackedLabel>
-        <LabelError label={this.props.label} obligatory={this.props.obligatory} />
-        <Input 
-          value={this.props.value} 
-          secureTextEntry={this.props.secureTextEntry} 
-          onChangeText={(v) => this.props.onChangeText(v)} 
-          keyboardType={this.props.keyboardType || 'default'}
-        />
-      </Item>
-    )
+    if (props.secureTextEntry) {
+      return (
+        <>
+          <Button dark danger={props.error} transparent 
+            //onPress={() => setHideValue(!hideValue)}
+            onPressIn={() => setHideValue(false)}
+            onPressOut={() => setHideValue(true)}
+          >
+            <Icon type="MaterialCommunityIcons" name={hideValue ? 'eye-off' : 'eye-outline'} />
+          </Button>
+        </>
+      )
+    }
+    if (props.error){
+      return (
+        <Button danger transparent>
+          <Icon type="MaterialCommunityIcons" name="close-circle" />
+        </Button>
+      )
+    }
   }
-
+  
+  return (
+    <Item stackedLabel>
+      <LabelError label={props.label} obligatory={props.obligatory} />
+      <Row>
+          <Input 
+            error={props.error || false}
+            disabled={props.disabled || false}
+            placeholder={props.placeholder} 
+            value={props.value} 
+            secureTextEntry={props.secureTextEntry && hideValue} 
+            onChangeText={(v) => props.onChangeText(v)} 
+            keyboardType={props.keyboardType || 'default'}
+          />
+          {getButton()}
+      </Row>
+    </Item>
+  )
 }
