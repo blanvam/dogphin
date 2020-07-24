248c9f7aa8bf604b92ae56594104884db429de12
Victor M. Macias Ariza <blanvam@gmail.com>
2020-07-24 16:57:53 +0200

    Now login is required to create emergency or alert

--- src/map/MapBar.js
+++ src/map/MapBar.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { connect } from 'react-redux'
 import { StyleSheet } from 'react-native'
-import { Icon, Button, View } from 'native-base'
+import { Icon, Button, View, Toast } from 'native-base'
 
 import * as alertActions from '../alert/alert.actions'
 import * as emergencyActions from '../emergency/emergency.actions'
@@ -21,16 +21,45 @@ const styles = StyleSheet.create({
 })
 
 const MapBar = props => {
+
+  showErrorAndGoLogin = msg => {
+    Toast.show({
+      text: msg,
+      //textStyle: { color: "red" },
+      type: "warning",
+      buttonText: "Vale",
+      duration: 6000,
+      position: "top"
+    })
+    props.navigation.navigate('Login')
+  }
+
+  showEmergencyModal = () => {
+    if (props.user.email) {
+      props.toggleEmergencyModal(true)
+    } else {
+      showErrorAndGoLogin('Debe logearse para crear una emergencia!') 
+    }
+  }
+
+  showAlertModal = () => {
+    if (props.user.email) {
+      props.toggleAlertModal(true)
+    } else {
+      showErrorAndGoLogin('Debe logearse para crear una alerta!')
+    }
+  }
+
   return (
     <View style={styles.mapBar}>
       <View style={styles.alertBar}>
-        <Button first rounded danger onPress={() => props.toggleEmergencyModal(true)} >
+        <Button first rounded danger onPress={showEmergencyModal} >
           <Icon type="Octicons" name="alert" style={{ fontSize: 30, color: 'white' }} />
         </Button>
         <Button block rounded success style={{ width: '55%', marginLeft: 20, marginRight: 20 }} >
           <LocationSwitch />
         </Button>
-        <Button last rounded onPress={() => props.toggleAlertModal(true)} style={{backgroundColor: 'white'}}>
+        <Button last rounded onPress={showAlertModal} style={{backgroundColor: 'white'}}>
           <Icon type="Octicons" name="issue-opened" style={{ fontSize: 30, color: 'orange' }} />
         </Button>
       </View>
@@ -38,8 +67,10 @@ const MapBar = props => {
   )
 }
 
-const mapStateToProps = _ => {
-  return {}
+const mapStateToProps = state => {
+  return {
+    user: state.user.user,
+  }
 }
 
 const mapDispatchToProps = dispatch => {
