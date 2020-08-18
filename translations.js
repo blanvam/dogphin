7e2520a48a1e12f20d2bd4c058bfb91d7b4325fd
Adrian Cepillo <adrian.cepillo@gmail.com>
2020-08-19 01:43:50 +0200

    Adds translations for FooterBar

---
+++ translations.js
@@ -0,0 +1,23 @@
+export default translations = {
+  en: {
+    translation: {
+      'map': 'Map',
+      'notifications': 'Notifications',
+      'weather': 'Weather'
+    }
+  },
+  es: {
+    translation: {
+      'map': 'Mapa',
+      'notifications': 'Notificaciones',
+      'weather': 'Tiempo'
+    }
+  },
+  fr: {
+    translation: {
+      'map': 'Carte',
+      'notifications': 'Météo',
+      'weather': 'Alertes en cours'
+    }
+  }
+}
