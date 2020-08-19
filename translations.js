92492bcff6bc919dd266d91e77405a07704ef0e0
Adrian Cepillo <adrian.cepillo@gmail.com>
2020-08-19 02:57:39 +0200

    Adds alerts translations and substitutes texts for translations in CreateAlertScreen

--- translations.js
+++ translations.js
@@ -1,29 +1,137 @@
 export default translations = {
   en: {
     translation: {
-      'map': 'Map',
-      'notifications': 'Notifications',
-      'weather': 'Weather',
-      'createAlertTitle': 'Notify an Alert',
-      'cancel': 'Cancel',
+      map: "Map",
+      notifications: "Notifications",
+      weather: "Weather",
+      createAlertTitle: "Notify an Alert",
+      cancel: "Cancel",
+      boatDamage: {
+        name: "Damaged Boat",
+        title: "Damaged Boat!",
+        description:
+          "Looks like someone's got a damaged boat! Can you help him?"
+      },
+      fuelEmpty: {
+        name: "Fuel Empty",
+        title: "Fuel Empty",
+        description: "Someone without fuel, get in touch if you can help."
+      },
+      badWeather: {
+        name: "Bad Weather",
+        title: "Bad weather",
+        description: ""
+      },
+      floatingObject: {
+        name: "Floating Object",
+        title: "Floating object",
+        description: ""
+      },
+      mechanicFailure: {
+        name: "Mechanical Failure",
+        title: "Mechanical failure",
+        description: ""
+      },
+      healthSos: {
+        name: "Health SOS",
+        title: "Health SOS!",
+        description: ""
+      },
+      emergency: {
+        name: "Emergency! SOS!!",
+        title: "Emergency! SOS!!",
+        description: ""
+      }
     }
   },
   es: {
     translation: {
-      'map': 'Mapa',
-      'notifications': 'Notificaciones',
-      'weather': 'Tiempo',
-      'createAlertTitle': 'Notificar una Alerta',
-      'cancel': 'Cancelar',
+      map: "Mapa",
+      notifications: "Notificaciones",
+      weather: "Tiempo",
+      createAlertTitle: "Notificar una Alerta",
+      cancel: "Cancelar",
+      boatDamage: {
+        name: "Barco dañado",
+        title: "¡Barco dañado!",
+        description:
+          "¡Parece que alguien tiene el barco dañado! ¿Puedes ayudarle?"
+      },
+      fuelEmpty: {
+        name: "Sin Combustible",
+        title: "Sin combustible",
+        description:
+          "¡Alguien sin combustible, ponte en contacto si puedes ayudar."
+      },
+      badWeather: {
+        name: "Mal tiempo",
+        title: "Mal tiempo/oleaje",
+        description: "Parece que hay oleaje o previsión de mal tiempo. ¡Tome precauciones!"
+      },
+      floatingObject: {
+        name: "Objeto peligroso",
+        title: "Objeto flotando/peligroso",
+        description: "¡Tienes cerca un objeto flotante que podría ser peligroso!"
+      },
+      mechanicFailure: {
+        name: "Fallo mecánico",
+        title: "Fallo mecánico",
+        description: "Cerca hay alguien con un fallo de motor o eléctrico"
+      },
+      healthSos: {
+        name: "Ayuda sanitaria",
+        title: "¡SOS! Problemas de salud",
+        description: "¡Alguien está teniendo problemas de salud! ¿Puedes ayudarle?"
+      },
+      emergency: {
+        name: "Emergencia",
+        title: "¡Emergencia! ¡¡SOS!!",
+        description: "Se necesita ayuda urgente!"
+      }
     }
   },
   fr: {
     translation: {
-      'map': 'Carte',
-      'notifications': 'Météo',
-      'weather': 'Alertes en cours',
-      'createAlertTitle': 'Alerte à la communauté',
-      'cancel': 'Annuler',
+      map: "Carte",
+      notifications: "Météo",
+      weather: "Alertes en cours",
+      createAlertTitle: "Alerte à la communauté",
+      cancel: "Annuler",
+      boatDamage: {
+        name: "Navire endommagé",
+        title: "Bateau endommagé",
+        description: "Un membre a endommagé son bateau sur cette zone"
+      },
+      fuelEmpty: {
+        name: "Panne carburant",
+        title: "Panne de carburant",
+        description: "Un membre est en panne de carburant sur cette zone"
+      },
+      badWeather: {
+        name: "",
+        title: "",
+        description: ""
+      },
+      floatingObject: {
+        name: "",
+        title: "",
+        description: ""
+      },
+      mechanicFailure: {
+        name: "",
+        title: "",
+        description: ""
+      },
+      healthSos: {
+        name: "",
+        title: "",
+        description: ""
+      },
+      emergency: {
+        name: "",
+        title: "",
+        description: ""
+      }
     }
   }
 }
